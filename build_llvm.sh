db00a576a4e61aa62b0a6e538b90fd056b83c791
Raphael Isemann <teemperor@gmail.com>
2017-09-05 10:15:56 +0200

    Increased link jobs to 2

--- build_llvm.sh
+++ build_llvm.sh
@@ -8,12 +8,12 @@ export PATH="/usr/lib/ccache/bin/:$PATH"
 export CCACHE_BASEDIR="`realpath ..`"
 echo "$CCACHE_BASEDIR"
 
-cmake -DCMAKE_C_FLAGS="-march=native -Wno-gnu-statement-expression" \
+cmake -DCMAKE_C_FLAGS="-march=native -O3 -Wno-gnu-statement-expression" \
       -DCMAKE_CXX_FLAGS="-march=native" \
       -DLLVM_USE_SANITIZER="Address;Undefined" \
       -DLLVM_ENABLE_EXPENSIVE_CHECKS=OFF -DLLVM_CCACHE_BUILD=OFF \
       -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=On \
-      -DLLVM_LIT_ARGS=" -v -j 3" -DLLVM_PARALLEL_LINK_JOBS=1 -GNinja $1
+      -DLLVM_LIT_ARGS=" -v -j 3" -DLLVM_PARALLEL_LINK_JOBS=2 -GNinja $1
 shift
 ninja $*
 
